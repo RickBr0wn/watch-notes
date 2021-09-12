76b9e04d32b0159b6b69cbc983a08800a13f7a63
Rick Brown <ricky.brown.00@gmail.com>
2021-09-12 11:01:43 +0100

    Initial Commit

---
+++ watch-notes WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watch-notes WatchKit Extension
+//
+//  Created by Rick Brown on 12/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watch-notes WatchKit Extension/watch_notesApp.swift
@@ -0,0 +1,19 @@
+//
+//  watch_notesApp.swift
+//  watch-notes WatchKit Extension
+//
+//  Created by Rick Brown on 12/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct watch_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
